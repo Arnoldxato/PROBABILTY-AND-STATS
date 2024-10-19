27dce280329dbf9f15dcba6e8972a54dedbb2811
ARNOLD XATO <muyoboarnold@gmail.com>
2024-10-19 10:55:37 +0300

    This is an over view of probability and statistics
    This has explanations of the basics of the creation of a juypter notebook

---
+++ PROB_OVERVIEW/First.py
@@ -0,0 +1,28 @@
+## python  BASICS
+#single line comment
+
+'''
+Muli line comment
+'''
+#decalring variables
+# Here we create different variables o
+name ='xato'
+number_one =12
+number_two = 3.14
+status=False
+
+print(type(number_one))
+print(type(number_two))
+print(type(status))
+print(number_one+number_two)
+
+def subtraction():
+    difference = number_one -number_two
+    return difference
+print(subtraction())
+
+## A void function
+def Addition():
+    sum = number_one+number_two
+    print(sum)
+Addition()
